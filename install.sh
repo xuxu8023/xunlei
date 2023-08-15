347c5c4fa0cceff9128918b91df58ea34955d5ef
xuxu8023 <129353218+xuxu8023@users.noreply.github.com>
2023-08-15 09:38:22 +0800

    Update install.sh
    3.11.2

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -eu
 
 latest=v2.6.1
 #latest=$(curl -fsSL https://api.github.com/repos/cnk3x/xunlei/releases/latest | grep browser_download_url | grep $(uname -m) | head -n 1 | grep -Eo https.+.tar.gz)
-latest=https://github.com/cnk3x/xunlei/releases/download/v2.6.1/xunlei-v2.6.1.linux.x86_64.tar.gz
+latest=https://github.com/xuxu8023/xunlei/releases/download/3.11.2/3.11.2.tar.gz
 echo "download: $latest"
 curl -fsSL ${latest} | tar zx
 ./xunlei $@
